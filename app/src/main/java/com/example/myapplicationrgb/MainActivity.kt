e9182efecde5ff53a67b127564873d94cd2619b0
SuneelM <mauryasuneel@outlook.com>
2022-07-24 10:00:45 -0400

    create a common object for all seekbar, change rootProject name

--- app/src/main/java/com/example/myapplicationrgb/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationrgb/MainActivity.kt
@@ -9,11 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.String
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var colorView:TextView
-    private lateinit var redColor:TextView
-    private lateinit var greenColor:TextView
-    private lateinit var blueColor:TextView
-    private lateinit var hexCode:TextView
+    private lateinit var colorView: TextView
+    private lateinit var redColor: TextView
+    private lateinit var greenColor: TextView
+    private lateinit var blueColor: TextView
+    private lateinit var hexCode: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,42 +28,34 @@ class MainActivity : AppCompatActivity() {
         blueColor = findViewById(R.id.blue_color)
         hexCode = findViewById(R.id.hex_code)
 
-        redSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                redColor.text = p1.toString()
-                setViewColor()
-            }
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
 
-        greenSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        val obj = object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                greenColor.text = p1.toString()
+                val seekbarView = p0?.let { findViewById<SeekBar>(it.id) }
+                when (seekbarView) {
+                    redSeekbar -> redColor.text = p1.toString()
+                    greenSeekbar -> greenColor.text = p1.toString()
+                    blueSeekbar -> blueColor.text = p1.toString()
+                }
                 setViewColor()
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
+        }
 
-        blueSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                blueColor.text = p1.toString()
-                setViewColor()
-            }
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
+        redSeekbar.setOnSeekBarChangeListener(obj)
+        greenSeekbar.setOnSeekBarChangeListener(obj)
+        blueSeekbar.setOnSeekBarChangeListener(obj)
 
     }
 
     @SuppressLint("SetTextI18n")
-    fun setViewColor(){
+    fun setViewColor() {
         val red = redColor.text.toString().toInt()
         val green = greenColor.text.toString().toInt()
         val blue = blueColor.text.toString().toInt()
-        colorView.setBackgroundColor(Color.rgb(red,green,blue))
-        hexCode.text = "Hex:"+String.format("#%02X%02X%02X", red, green, blue)
+        colorView.setBackgroundColor(Color.rgb(red, green, blue))
+        hexCode.text = "Hex:" + String.format("#%02X%02X%02X", red, green, blue)
 
     }
 
