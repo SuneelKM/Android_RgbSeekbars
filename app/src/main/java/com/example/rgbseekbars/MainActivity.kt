836aea381b512e630229aeaf46152643e9b076e6
SuneelM <mauryasuneel@outlook.com>
2022-07-30 17:13:00 -0400

    Add material design and sliders

--- app/src/main/java/com/example/rgbseekbars/MainActivity.kt
+++ app/src/main/java/com/example/rgbseekbars/MainActivity.kt
@@ -2,42 +2,42 @@ package com.example.rgbseekbars
 
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rgbseekbars.databinding.ActivityMainBinding
+import com.google.android.material.slider.Slider
 import kotlin.String
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val obj = object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                val colorTextView = when (p0?.id) {
-                    binding.redSeekBar.id -> binding.redColor
-                    binding.greenSeekBar.id -> binding.greenColor
-                    else -> binding.blueColor
-                }
-                colorTextView.text = p1.toString()
-                setViewColor()
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) = Unit
-            override fun onStopTrackingTouch(p0: SeekBar?) = Unit
+        binding.redSlider.addOnChangeListener { slider, value, _ ->
+            addColor(slider, value)
         }
-
-        binding.redSeekBar.setOnSeekBarChangeListener(obj)
-        binding.greenSeekBar.setOnSeekBarChangeListener(obj)
-        binding.blueSeekBar.setOnSeekBarChangeListener(obj)
-
+        binding.greenSlider.addOnChangeListener { slider, value, _ ->
+            addColor(slider, value)
+        }
+        binding.blueSlider.addOnChangeListener { slider, value, _ ->
+            addColor(slider, value)
+        }
     }
 
-    fun setViewColor() {
+    private fun addColor(slider: Slider, value: Float) {
+        val colorTextView = when (slider.id) {
+            binding.redSlider.id -> binding.redColor
+            binding.greenSlider.id -> binding.greenColor
+            else -> binding.blueColor
+        }
+        colorTextView.text = value.toInt().toString()
+        setViewColor()
+    }
+
+    private fun setViewColor() {
         val red = binding.redColor.text.toString().toInt()
         val green = binding.greenColor.text.toString().toInt()
         val blue = binding.blueColor.text.toString().toInt()
